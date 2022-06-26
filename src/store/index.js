ac01885289cd43d953e286a3a941c74569fcc425
Dome <dome@peerapongs-MacBook-Air.local>
2022-06-26 15:07:00 +0700

    add vuex-persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,21 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersistence from 'vuex-persist'
 import AuthModule from './modules/auth'
 
 Vue.use(Vuex)
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage,
+  key: 'vue-authen',
+  modules: ['auth']
+})
 
 const store = new Vuex.Store({
   modules: {
     auth: AuthModule
   },
-  plugins: []
+  plugins: [vuexLocal.plugin]
 })
 
 export default store
